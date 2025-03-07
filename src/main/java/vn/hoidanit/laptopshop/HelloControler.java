ce33dc31de394e18ef76a78622735c3d66dd03e9
sonbon2000 <sonbon1101@gmail.com>
2025-03-07 10:33:50 +0700

    video 17

---
+++ src/main/java/vn/hoidanit/laptopshop/HelloControler.java
@@ -0,0 +1,13 @@
+package vn.hoidanit.laptopshop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloControler {
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from hoi dan IT!";
+    }
+
+}
